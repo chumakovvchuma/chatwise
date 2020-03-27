88822df0eb08e7c8c25c53a959b1fc528e3e3d3d
Victor Chumakov <victorchumakov@MacBook-Air-Victor.local>
2020-03-27 18:50:17 +0200

    first committ

---
+++ build/precache-manifest.cc03f444199f889e3732a3e846dfdfce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbdaa6c772e09626e17f033cf105dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "646ba05ce6aa429f241b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a949a5e126b82581d1b5",
+    "url": "/static/js/2.d99834db.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d99834db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646ba05ce6aa429f241b",
+    "url": "/static/js/main.b65313da.chunk.js"
+  },
+  {
+    "revision": "b6e9fddc340a9a7d8031",
+    "url": "/static/js/runtime-main.74caae67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
         </a>
+        
       </header>
     </div>
   );
